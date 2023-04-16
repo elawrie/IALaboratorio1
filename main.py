513c5186c022df3a56866e947722bba0e39fea55
Evelyn Lawrie <lawrie@chapman.edu>
2023-04-16 13:16:45 -0400

    agrega la funcion expandnode

--- main.py
+++ main.py
@@ -33,13 +33,10 @@ def main():
     # prueba la funcion expand_node
     hijos = []
     hijos = expand_node(init_state)
+    
     # imprime todos los hijos posibles del estado inicial 
-    # FIXME: los matrices de los hijos no imprimen correctamente 
-    print("hijo 1:\n", print_matrix(hijos[0]))
-    print("hijo n:\n", print_matrix(hijos[1]))
-
-    # FIXME: eso dice que los hijos son iguales - algo esta mal con expand_node
-    print("are equal: ", isgoal(hijos[0], hijos[1]))
+    # print("are equal: ", isgoal(hijos[0], hijos[2]))
+    # print(hijos)
 
 if __name__ == "__main__":
     main()
